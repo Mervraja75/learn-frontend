15d741216c1a0c800564fb3ce001042ed5ee6c4b
Mervin Raja <158093307+Mervraja75@users.noreply.github.com>
2026-05-12 23:08:02 -1000

    first commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -18,7 +18,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Hi, I'm Mervin Raja — learning React Native!!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
